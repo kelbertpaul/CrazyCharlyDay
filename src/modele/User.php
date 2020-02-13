16dca32984c84d2e7a5e38a67a002fb4df3aa55e
Paul KELBERT <paul.kelbert3@etu.univ-lorraine.fr>
2020-02-13 12:18:32 +0100

    Nouveau site

--- src/modele/User.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace CrazyCharlyDay\modele;
-class User extends \Illuminate\Datebase\Eloquent\Model {
-    protected $table = 'user';
-    protected $primaryKey = 'id';
-    public $timestamp = false;
-}
