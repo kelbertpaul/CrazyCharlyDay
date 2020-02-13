930408b054f00c06d7ecdba28169e69e9047055e
Paul KELBERT <paul.kelbert3@etu.univ-lorraine.fr>
2020-02-13 11:43:23 +0100

    bootstrap

--- src/vue/VueConnexion.php
+++ src/vue/VueConnexion.php
@@ -2,16 +2,33 @@
 
 namespace crazy\vue;
 
+use Slim\Slim;
+
 class VueConnexion {
     private $html;
 
     public $selector;
+    /**
+     * @var string
+     */
+    private $URLbootstrapCSS;
+    /**
+     * @var string
+     */
+    private $URLbootstrapJS;
+    /**
+     * @var Slim|null
+     */
+    private $app;
+
     public function __construct($sel) {
     $this->selector = $sel;
+        $this->app = Slim::getInstance();
+        $this->URLbootstrapCSS = $this->app->request->getRootUri() . '/public/css/bootstrap.css';
+        $this->URLbootstrapJS = $this->app->request->getRootUri() . '/public/js/boostrap.min.js';
     }
     public function formulaireCo() {
-    $html =
-            <<<END
+    $html = <<<END
             <html lang="en">
                     <head>
                       <meta charset="utf-8">
@@ -20,7 +37,10 @@ class VueConnexion {
                       <meta name="author" content="">
                       <title>Bare - Start Bootstrap Template</title>
                       <!-- Bootstrap core CSS -->
-                      <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+                      <link rel="stylesheet" href="$this->URLbootstrapCSS">
+                      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+                    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
                     </head>
                     <body>
                       <!-- Navigation -->
@@ -68,11 +88,10 @@ class VueConnexion {
                         </form>
                       </div>
                       <!-- Bootstrap core JavaScript -->
-                      <script src="vendor/jquery/jquery.slim.min.js"></script>
-                      <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-                    </body>
-                    </html>
-    END;
+                      <script src="$this->URLbootstrapJS"></script>
+            </body>
+        </html> 
+END;
     $this->html = $html;
     }
 
